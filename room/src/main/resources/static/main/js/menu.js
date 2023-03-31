3fedb4c7a175e658e3d8ed1c860dd047f0671c48
selre1 <selre1@naver.com>
2023-03-31 11:53:33 +0900

    [MOD] 메인페이지 변경

---
+++ room/src/main/resources/static/main/js/menu.js
@@ -0,0 +1,13 @@
+// 사이드바(메뉴)를 열고 닫을 때
+    function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+    function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
+
+
+
